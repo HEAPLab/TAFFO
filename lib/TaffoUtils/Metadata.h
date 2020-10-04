57fb2e1e901e79263c92dd2b5a18249bd3752216
klaa97 <10521251@polimi.it>
2020-10-04 23:11:22 +0200

    Add OpenMP indirect metadata
    Use generalized metadata

--- lib/TaffoUtils/Metadata.h
+++ lib/TaffoUtils/Metadata.h
@@ -41,6 +41,7 @@
 #define ORIGINAL_FUN_METADATA  "taffo.originalCall"
 #define CLONED_FUN_METADATA    "taffo.equivalentChild"
 #define SOURCE_FUN_METADATA    "taffo.sourceFunction"
+#define INDIRECT_METADATA      "taffo.indirectFunction"
 
 /* Integer which specifies the distance of the metadata from the
  * original annotation as data flow node counts.
